fcc6b33174aba3748a0ce3982ccbbe1473970f49
12691282 <12691282@qq.com>
2019-07-20 17:14:41 +0800

    init build
    init build

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button >+</button>
+    <button >-</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
